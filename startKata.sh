ae5112f77dc00fc2144d012ddb08d38d9f7174fe
KroneckerDelta <kroneckerdelta@thomasmichael.de>
2012-09-13 21:18:58 +0200

    kata: one_wrong_commit. Delete one commit.

---
+++ startKata.sh
@@ -0,0 +1,26 @@
+echo "Some Work in progress on master" > c1.txt
+git add c1.txt
+git commit -m "First commit of some on going work"
+
+echo "Some good Work" >> c1.txt
+git add c1.txt
+git commit -m "Some good Work."
+
+echo "Some very good Work" >> c1.txt
+git add c1.txt
+git commit -m "Some good Work."
+
+echo "bullshit" >> c1.txt
+git add c1.txt
+git commit -m "This could be wrong..."
+
+echo "Some very good Work" >> c1.txt
+git add c1.txt
+git commit -m "Some good Work."
+
+echo "Some good Work" >> c1.txt
+git add c1.txt
+git commit -m "Finish the work"
+
+
+cat readMe.txt
