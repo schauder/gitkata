ebbc850bcbdaf1bc6b3d91b5f0c5af497c1ce99c
KroneckerDelta <kroneckerdelta@thomasmichael.de>
2012-09-17 20:13:49 +0200

    created kata kata_too_many_commits_reduce_to_one

---
+++ readMe.txt
@@ -0,0 +1,15 @@
+#########################################################
+#							#
+# Hello, this kata calls: 				#
+#							#
+# 'too many commits reduce to one'			#
+#							#
+# There are to many commits for a Feature		#
+#							# 
+# Reduce whole commits to one commit only		# 
+#							# 
+# Git! Git! Git!					# 
+#							# 
+# This kata was made by KroneckerDelta			# 
+#							# 
+#########################################################
---
+++ startKata.sh
@@ -0,0 +1,26 @@
+echo "This " > c1.txt
+git add c1.txt
+git commit -m "Finish with work!"
+
+echo "should " >> c1.txt
+git add c1.txt
+git commit -m "Finish, now"
+
+echo "be " >> c1.txt
+git add c1.txt
+git commit -m "Forgot something, now finish."
+
+echo "one " >> c1.txt
+git add c1.txt
+git commit -m "Sorry, sorry, sorry, Now my work is done!"
+
+echo "commit " >> c1.txt
+git add c1.txt
+git commit -m "I forgot brain!! Now its finish."
+
+echo "only." >> c1.txt
+git add c1.txt
+git commit -m "Added new File with desired content."
+
+
+cat readMe.txt
